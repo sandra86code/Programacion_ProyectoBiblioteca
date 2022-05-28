4c2fd554b5ebd30762dd1febed680ca7353faad6
sandra <sandra.ruizjimenez86@gmail.com>
2022-05-28 13:35:37 +0200

    Ejercicio terminado

--- Biblioteca/src/com/jacaranda/main/PrincipalBBDD.java
+++ Biblioteca/src/com/jacaranda/main/PrincipalBBDD.java
@@ -3,17 +3,13 @@ package com.jacaranda.main;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.jacaranda.biblioteca.Biblioteca;
 import com.jacaranda.biblioteca.BibliotecaException;
-import com.jacaranda.recursos.AutorException;
-import com.jacaranda.recursos.RecursoException;
 
 public class PrincipalBBDD {
 	
@@ -25,13 +21,6 @@ public class PrincipalBBDD {
 		String nombreBiblio;
 		int opc;
 		int subopcion;
-		//Datos del lector
-		String nombrePila;
-		String apellidos;
-		String dni;
-		String movil;
-		String direccion;
-		String email;
 		//Datos autor
 		String nombre;
 		String nacionalidad;
@@ -43,14 +32,12 @@ public class PrincipalBBDD {
 		LocalDate fechaPublicacion;
 		String periodicidad;
 		String genero;
-		//Datos ejemplar
-		String estado;
-		String localizacion;
-		
+
 		
 		//BBDD
+		Connection conexion = null;
 		try {
-			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_biblioteca", "root", "root");
+			conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/db_biblioteca", "root", "root");
 
 			nombreBiblio = leerCadena("Introduce el nombre de la biblioteca: ");
 			Biblioteca biblio = null;
@@ -100,15 +87,8 @@ public class PrincipalBBDD {
 						//Inserto en la tabla recurso
 						query = "INSERT INTO RECURSO VALUES ('" + codigo + "','" + titulo + "','" + editorial + "','" + fechaPublicacion + "');";
 						instruccion.executeUpdate(query);
-						//Obtengo el código para esa periodicidad
-						query2 = "SELECT CODIGO FROM PERIODICIDADREVISTA WHERE " + "'" + periodicidad + "'" + " = PERIODICIDAD;";
-						ResultSet resultados = instruccion.executeQuery(query2);
-						int codigoPeriodicidad = 0;
-						while(resultados.next()) {
-							codigoPeriodicidad = resultados.getInt("CODIGO");
-						}
-						//Inserto en la tabla revista
-						query3 = "INSERT INTO REVISTA VALUES ('" + codigo + "','" + codigoPeriodicidad + "');";
+						//Inserto en la tabla libro
+						query3 = "INSERT INTO REVISTA VALUES ('" + codigo + "', (SELECT CODIGO FROM PERIODICIDADREVISTA WHERE PERIODICIDAD = '" + periodicidad + "'));";
 						instruccion.executeUpdate(query3);
 						//Inserto en la tabla recursoautor
 						query4 = "INSERT INTO RECURSOAUTOR VALUES ('" + nombre + "','" + codigo + "');";
@@ -133,15 +113,8 @@ public class PrincipalBBDD {
 						//Inserto en la tabla recurso
 						query = "INSERT INTO RECURSO VALUES ('" + codigo + "','" + titulo + "','" + editorial + "','" + fechaPublicacion + "');";
 						instruccion.executeUpdate(query);
-						//Obtengo el código para ese genero
-						query2 = "SELECT CODIGO FROM GENEROLIBRO WHERE " + "'" + genero + "'" + " = GENERO;";
-						ResultSet resultados2 = instruccion.executeQuery(query2);
-						int codigoGenero = 0;
-						while(resultados2.next()) {
-							codigoGenero = resultados2.getInt("CODIGO");
-						}
 						//Inserto en la tabla libro
-						query3 = "INSERT INTO LIBRO VALUES ('" + codigo + "','" + codigoGenero + "');";
+						query3 = "INSERT INTO LIBRO VALUES ('" + codigo + "', (SELECT CODIGO FROM GENEROLIBRO WHERE GENERO = '" + genero + "'));";
 						instruccion.executeUpdate(query3);
 						//Inserto en la tabla recursoautor
 						query4 = "INSERT INTO RECURSOAUTOR VALUES ('" + nombre + "','" + codigo + "');";
@@ -154,35 +127,13 @@ public class PrincipalBBDD {
 				case 4: //Borrar un autor
 					nombre = leerCadena("Introduce el nombre y apellidos del autor: ").toUpperCase();
 					try {
-						Statement instruccion2 = conexion.createStatement();
-						Statement instruccion3 = conexion.createStatement();
-						//Busco los códigos de los recursos asociados a ese autor en la tabla RECURSO_AUTOR
-						query = "SELECT CODIGO_RECURSO FROM RECURSOAUTOR WHERE NOMBRE_AUTOR = " + "'" + nombre + "';";
-						ResultSet resultados3 = instruccion3.executeQuery(query);
-						
-						//Creo un array para poder añadir todos los códigos ya que no puedo iterar sobre el ResultSet y a la vez hacerle
-						//el delete a la otra tabla
-						ArrayList<String> codigosRecursos = new ArrayList<>();
-						
-						//Recorro el ResultSet y añado cada valor al ArrayList
-						ResultSetMetaData infoResultados = resultados3.getMetaData();
-						int col = infoResultados.getColumnCount();
-						System.out.println(col);
-						while (resultados3.next()) {
-							for (int i = 1; i <= col; i++) {
-								codigosRecursos.add(resultados3.getString(i));
-							}
+						instruccion = conexion.createStatement();
+						query = "DELETE AUTOR, RECURSO FROM AUTOR, RECURSO, RECURSOAUTOR WHERE AUTOR.NOMBRE = " + "'" + nombre + "'" + " AND AUTOR.NOMBRE = RECURSOAUTOR.NOMBRE_AUTOR AND RECURSOAUTOR.CODIGO_RECURSO = RECURSO.CODIGO;";
+						if(instruccion.executeUpdate(query)==0) {
+							System.out.println("Autor no encontrado.");
+						}else {
+							System.out.println("Autor borrado correctamente.");
 						}
-						//Recorro el array y ejecuto el query delete en cada iteración con el valor adecuado para cada recurso.
-						//El on delete cascade de RecursoAutor me va eliminar automáticamente todos los recursos asociados al autor
-						for(String recurso : codigosRecursos) {
-							query2 = "DELETE FROM RECURSO WHERE CODIGO = '" + recurso + "';";
-							instruccion3.executeUpdate(query2);
-						}
-						
-						//Por último, borro al autor de la tabla autor
-						query3 = "DELETE FROM AUTOR WHERE NOMBRE = '" + nombre + "';";
-						instruccion3.executeUpdate(query3);
 					}catch(Exception e) {
 						System.out.println("No se ha podido borrar el autor: " + e.getMessage());
 					}
@@ -192,7 +143,11 @@ public class PrincipalBBDD {
 					try {
 						instruccion = conexion.createStatement();
 						query = "DELETE FROM RECURSO WHERE CODIGO = '" + codigo + "';";
-						instruccion.executeUpdate(query);
+						if(instruccion.executeUpdate(query)==0) {
+							System.out.println("No existe el código.");
+						}else {
+							System.out.println("Se ha eliminado el recurso.");
+						}
 					}catch(Exception e) {
 						System.out.println("No se ha podido borrar el recurso: " + e.getMessage());
 					}
@@ -200,14 +155,19 @@ public class PrincipalBBDD {
 				case 6: //Modificar un autor
 					menuModificaAutor();
 					subopcion = leerEntero("Introduce la opción deseada: ");
+					nombre = leerCadena("Introduce el nombre y apellidos del autor: ").toUpperCase();
 					switch (subopcion) {
 					case 1:
 						nacionalidad = leerCadena("Introduce la nacionalidad del autor: ").toUpperCase();
 						try {
 							//Modificar en la base de datos
 							instruccion = conexion.createStatement();
-							query = "UPDATE AUTOR SET NACIONALIDAD = " + nacionalidad + ";";
-							instruccion.executeUpdate(query);
+							query = "UPDATE AUTOR SET NACIONALIDAD = '" + nacionalidad + "' WHERE NOMBRE = " + "'" + nombre + "';";
+							if(instruccion.executeUpdate(query)==0) {
+								System.out.println("No existe un autor con ese nombre.");
+							}else {
+								System.out.println("Nacionalidad modificada.");
+							}
 						}catch(Exception e) {
 							System.out.println("No se ha podido modificar la nacionalidad del autor: " + e.getMessage());
 						}
@@ -217,8 +177,12 @@ public class PrincipalBBDD {
 						try {
 							//Modificar en la base de datos
 							instruccion = conexion.createStatement();
-							query = "UPDATE AUTOR SET FECHANACIMIENTO = " + fechaNacimiento + ";";
-							instruccion.executeUpdate(query);
+							query = "UPDATE AUTOR SET FECHANACIMIENTO = '" + fechaNacimiento + "' WHERE NOMBRE = " + "'" + nombre + "';";
+							if(instruccion.executeUpdate(query)==0) {
+								System.out.println("No existe un autor con ese nombre.");
+							}else {
+								System.out.println("Fecha de nacimiento modificada.");
+							}
 						}catch(Exception e) {
 							System.out.println("No se ha podido modificar la fecha de nacimiento del autor: " + e.getMessage());
 						}
@@ -231,36 +195,49 @@ public class PrincipalBBDD {
 				case 7: //Modificar un recurso
 					menuModificaRecurso();
 					subopcion = leerEntero("Introduce la opción deseada: ");
+					codigo = leerCadena("Introduce el ISSN o ISBN del recurso: ").toUpperCase();
 					switch (subopcion) {
 					case 1:
-						titulo = leerCadena("Introduce el título del libro: ").toUpperCase();
+						titulo = leerCadena("Introduce el título del recurso: ").toUpperCase();
 						try {
 							//Modificar en la base de datos
 							instruccion = conexion.createStatement();
-							query = "UPDATE RECURSO SET TITULO = " + titulo + ";";
-							instruccion.executeUpdate(query);
+							query = "UPDATE RECURSO SET TITULO = '" + titulo + "' WHERE CODIGO = " + "'" + codigo + "';";
+							if(instruccion.executeUpdate(query)==0) {
+								System.out.println("No existe un recurso con ese código.");
+							}else {
+								System.out.println("Título modificado.");
+							}
 						}catch(Exception e) {
 							System.out.println("No se ha podido modificar el titulo del recurso: " + e.getMessage());
 						}
 						break;
 					case 2:
-						editorial = leerCadena("Introduce el nombre de la editorial que publica el libro: ").toUpperCase();
+						editorial = leerCadena("Introduce el nombre de la editorial que publica el recurso: ").toUpperCase();
 						try {
 							//Modificar en la base de datos
 							instruccion = conexion.createStatement();
-							query = "UPDATE RECURSO SET EDITORIAL = " + editorial + ";";
-							instruccion.executeUpdate(query);
+							query = "UPDATE RECURSO SET EDITORIAL = '" + editorial + "' WHERE CODIGO = " + "'" + codigo + "';";
+							if(instruccion.executeUpdate(query)==0) {
+								System.out.println("No existe un recurso con ese código.");
+							}else {
+								System.out.println("Editorial modificada.");
+							}
 						}catch(Exception e) {
 							System.out.println("No se ha podido modificar la editorial del recurso: " + e.getMessage());
 						}
 						break;
 					case 3:
-						fechaPublicacion = leerFecha("publicación del libro");
+						fechaPublicacion = leerFecha("publicación del recurso");
 						try {
 							//Modificar en la base de datos
 							instruccion = conexion.createStatement();
-							query = "UPDATE RECURSO SET FECHAPUBLICACION = " + fechaPublicacion + ";";
-							instruccion.executeUpdate(query);
+							query = "UPDATE RECURSO SET FECHAPUBLICACION = '" + fechaPublicacion + "' WHERE CODIGO = " + "'" + codigo + "';";
+							if(instruccion.executeUpdate(query)==0) {
+								System.out.println("No existe un recurso con ese código.");
+							}else {
+								System.out.println("Fecha de publicación modificada.");
+							}
 						}catch(Exception e) {
 							System.out.println("No se ha podido modificar la fecha de publicación del recurso: " + e.getMessage());
 						}
@@ -272,23 +249,22 @@ public class PrincipalBBDD {
 					break;
 				case 8: //Listar las obras de un autor.
 					nombre = leerCadena("Introduce el nombre y apellidos del autor: ").toUpperCase();
-					instruccion = conexion.createStatement();
-					//Obtengo los resultados para ese autor de tabla recursoAutor
-					query = "SELECT * FROM RECURSO WHERE " + "'" + nombre + "'" + " = NOMBRE_AUTOR;";
-					ResultSet resultados2 = instruccion.executeQuery(query);
-
-					ArrayList<String> codigosRecursos2 = new ArrayList<>();
-					
-					ResultSetMetaData infoResultados2 = resultados2.getMetaData();
-					int col2 = infoResultados2.getColumnCount();
-					while (resultados2.next()) {
-						for (int i = 1; i <= col2; i++)
-							codigosRecursos2.add(resultados2.getString(i));
-					}
-					for(String codigoRecurso : codigosRecursos2) {
+					try {
+						instruccion = conexion.createStatement();
 						
+						query = "SELECT RECURSO.CODIGO, RECURSO.TITULO, RECURSO.EDITORIAL, RECURSO.FECHAPUBLICACION FROM RECURSO, RECURSOAUTOR WHERE RECURSOAUTOR.NOMBRE_AUTOR = " + "'" + nombre + "'" + " AND RECURSOAUTOR.CODIGO_RECURSO = RECURSO.CODIGO;";
+						
+						ResultSet resultados2 = instruccion.executeQuery(query);
+
+						while (resultados2.next()) {
+							System.out.println("Código: " + resultados2.getString("CODIGO") + ". Título: " + 
+									resultados2.getString("TITULO") + ". Editorial: " + resultados2.getString("EDITORIAL") +
+									". Fecha de publicación: " + resultados2.getString("FECHAPUBLICACION"));
+						}
+
+					}catch(Exception e) {
+						System.out.println("Error: " + e.getMessage());
 					}
-					
 					break;
 				default:
 					if(opc!=9) {
@@ -298,10 +274,21 @@ public class PrincipalBBDD {
 				}
 			}while(opc!=9);
 
-			System.out.println("Hasta la próxima");
 
 		} catch (SQLException e) {
 			System.out.println(e.getMessage());
+		}
+		
+		finally {
+			try {
+				if (conexion != null) {
+					// liberar la conexión a la BD
+					conexion.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Conexión cerrada.");
+			}
 		}
 	}
 	
